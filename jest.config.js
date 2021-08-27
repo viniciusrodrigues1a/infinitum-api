a3084b7d55ccb2334f8372f66acc39378aec0f98
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-27 07:00:35 -0300

    mapeando paths no jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 const { resolve } = require("path");
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig.json");
 const rootDir = resolve(__dirname);
 
 module.exports = {
@@ -9,4 +11,7 @@ module.exports = {
   testEnvironment: "node",
   clearMocks: true,
   testMatch: ["<rootDir>/src/**/*.test.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
 };
