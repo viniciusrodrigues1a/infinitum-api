60b35422c6f57110765833c3567d8a49f703d0e5
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-28 16:06:45 -0300

    fazendo hash e comparação de senhas usando pbkdf2

---
+++ src/modules/account/infra/cryptography/Pbkdf2.ts
@@ -0,0 +1,27 @@
+import crypto from "crypto";
+
+export type Pbkdf2Hash = {
+  hash: string;
+  salt: string;
+  iterations: number;
+};
+
+export class Pbkdf2 {
+  hash(plaintext: string): Pbkdf2Hash {
+    const salt = crypto.randomBytes(128).toString("base64");
+    const iterations = 20000;
+    const hash = crypto
+      .pbkdf2Sync(plaintext, salt, iterations, 64, "sha512")
+      .toString("hex");
+
+    return { hash, salt, iterations };
+  }
+
+  compare(plaintext: string, savedHash: Pbkdf2Hash): boolean {
+    const newHash = crypto
+      .pbkdf2Sync(plaintext, savedHash.salt, savedHash.iterations, 64, "sha512")
+      .toString("hex");
+
+    return savedHash.hash === newHash;
+  }
+}
---
+++ src/modules/account/infra/cryptography/index.ts
@@ -0,0 +1,1 @@
+export * from "./Pbkdf2";
