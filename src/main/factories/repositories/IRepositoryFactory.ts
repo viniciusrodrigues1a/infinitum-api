e655788ab97bc9e4d3a16f0f62cfe3d169b2baac
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-29 02:22:09 -0300

    adicionando factory pra instanciar novo repositorio

--- src/main/factories/repositories/IRepositoryFactory.ts
+++ src/main/factories/repositories/IRepositoryFactory.ts
@@ -1,5 +1,7 @@
 import { IRegisterAccountRepository } from "@modules/account/infra/repositories";
+import { IDoesAccountExistRepository } from "@modules/account/use-cases/interfaces/repositories";
 
 export interface IRepositoryFactory {
   makeRegisterAccountRepository(): IRegisterAccountRepository;
+  makeDoesAccountExistRepository(): IDoesAccountExistRepository;
 }
--- src/main/factories/repositories/KnexRepositoryFactoryImpl.ts
+++ src/main/factories/repositories/KnexRepositoryFactoryImpl.ts
@@ -1,10 +1,16 @@
 import {
   IRegisterAccountRepository,
+  KnexDoesAccountExistRepository,
   KnexRegisterAccountRepository,
 } from "@modules/account/infra/repositories";
+import { IDoesAccountExistRepository } from "@modules/account/use-cases/interfaces/repositories";
 import { IRepositoryFactory } from "./IRepositoryFactory";
 
 class KnexRepositoryFactoryImpl implements IRepositoryFactory {
+  makeDoesAccountExistRepository(): IDoesAccountExistRepository {
+    return new KnexDoesAccountExistRepository();
+  }
+
   makeRegisterAccountRepository(): IRegisterAccountRepository {
     return new KnexRegisterAccountRepository();
   }
