9bdafc0487f6dd3d17ea763f274fabb8b9f874d3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-29 03:54:20 -0300

    criando classe ExpressServer; rodando server em src/main/index.ts

---
+++ src/main/index.ts
@@ -0,0 +1,5 @@
+import { ExpressServer } from "./server";
+
+const server = new ExpressServer();
+
+server.start();
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,16 +1,33 @@
 import dotenv from "dotenv";
+import { Server } from "http";
 import "@shared/infra/database/connection";
 import express from "express";
 import { accountRoutes } from "./routes";
 
 dotenv.config();
 
-const app = express();
+export class ExpressServer {
+  app = express();
+  server: Server | null = null;
 
-app.use(express.json());
+  constructor() {
+    this.useMiddlewares();
+    this.useRoutes();
+  }
 
-app.use("/accounts", accountRoutes);
+  private useMiddlewares() {
+    this.app.use(express.json());
+  }
 
-const port = 3333;
-/* eslint-disable-next-line no-console  */
-app.listen(port, () => console.log(`Server running on port ${port}`));
+  private useRoutes() {
+    this.app.use("/accounts", accountRoutes);
+  }
+
+  public start(): void {
+    const port = process.env.PORT;
+    this.server = this.app.listen(port, () =>
+      /* eslint-disable-next-line no-console  */
+      console.log(`Server running on port ${port}`)
+    );
+  }
+}
