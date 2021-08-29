73957b874463cb6419a9a8370ab06418a440872a
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-08-29 03:53:04 -0300

    arrumando testes do controller

--- src/modules/account/presentation/controllers/CreateAccountController.test.ts
+++ src/modules/account/presentation/controllers/CreateAccountController.test.ts
@@ -1,6 +1,7 @@
 import { InvalidEmailError } from "@modules/account/entities/errors";
-import { CreateAccountUseCase } from "@modules/account/use-cases";
+import { IRegisterAccountRepository } from "@modules/account/infra/repositories";
 import { EmailAlreadyInUseError } from "@modules/account/use-cases/errors";
+import { IDoesAccountExistRepository } from "@modules/account/use-cases/interfaces/repositories";
 import { HttpStatusCodes } from "@shared/presentation/http/HttpStatusCodes";
 import { mock } from "jest-mock-extended";
 import {
@@ -9,10 +10,14 @@ import {
 } from "./CreateAccountController";
 
 function makeSut() {
-  const createAccountUseCaseMock = mock<CreateAccountUseCase>();
-  const sut = new CreateAccountController(createAccountUseCaseMock);
+  const registerAccountRepositoryMock = mock<IRegisterAccountRepository>();
+  const doesAccountExistRepositoryMock = mock<IDoesAccountExistRepository>();
+  const sut = new CreateAccountController(
+    registerAccountRepositoryMock,
+    doesAccountExistRepositoryMock
+  );
 
-  return { sut, createAccountUseCaseMock };
+  return { sut, registerAccountRepositoryMock, doesAccountExistRepositoryMock };
 }
 
 describe("createAccount controller", () => {
@@ -34,13 +39,13 @@ describe("createAccount controller", () => {
   it("should return HttpStatusCodes.serverError when unhandled error is thrown", async () => {
     expect.assertions(1);
 
-    const { sut, createAccountUseCaseMock } = makeSut();
+    const { sut, registerAccountRepositoryMock } = makeSut();
     const request: CreateAccountControllerRequest = {
       name: "Jorge",
       email: "jorge@email.com",
       password: "pa55",
     };
-    createAccountUseCaseMock.create.mockImplementationOnce(() => {
+    registerAccountRepositoryMock.create.mockImplementationOnce(() => {
       throw new Error("Supposedly unhandled server side error");
     });
 
@@ -49,38 +54,34 @@ describe("createAccount controller", () => {
     expect(response.statusCode).toBe(HttpStatusCodes.serverError);
   });
 
-  it("should return HttpStatusCodes.badRequest when EmailAlreadyInUseError is thrown", async () => {
+  it("should return HttpStatusCodes.badRequest when Account already exists", async () => {
     expect.assertions(2);
 
-    const { sut, createAccountUseCaseMock } = makeSut();
+    const { sut, doesAccountExistRepositoryMock } = makeSut();
     const request: CreateAccountControllerRequest = {
       name: "Jorge",
       email: "jorge@email.com",
       password: "pa55",
     };
-    const errorThrown = new EmailAlreadyInUseError(request.email);
-    createAccountUseCaseMock.create.mockImplementationOnce(() => {
-      throw errorThrown;
-    });
+    doesAccountExistRepositoryMock.doesAccountExist.mockResolvedValueOnce(true);
 
     const response = await sut.handleRequest(request);
 
     expect(response.statusCode).toBe(HttpStatusCodes.badRequest);
-    expect(response.body).toStrictEqual(errorThrown);
+    expect(response.body).toStrictEqual(
+      new EmailAlreadyInUseError(request.email)
+    );
   });
 
   it("should return HttpStatusCodes.badRequest when InvalidEmailError is thrown", async () => {
     expect.assertions(2);
 
-    const { sut, createAccountUseCaseMock } = makeSut();
+    const { sut } = makeSut();
     const request: CreateAccountControllerRequest = {
       name: "Jorge",
       email: "notanemail",
       password: "pa55",
     };
-    createAccountUseCaseMock.create.mockImplementationOnce(() => {
-      throw new InvalidEmailError();
-    });
 
     const response = await sut.handleRequest(request);
 
