1a885dab5e4c774667ae450670650f5a12b780b8
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-09-03 11:30:21 -0300

    testando rota de login

--- tests/routes/auth.test.ts
+++ tests/routes/auth.test.ts
@@ -1,4 +1,8 @@
-import { connection, configuration } from "@shared/infra/database/connection";
+import {
+  reconnect,
+  connection,
+  configuration,
+} from "@shared/infra/database/connection";
 import { api } from "../helpers";
 
 describe("account registering endpoint", () => {
@@ -53,6 +57,68 @@ describe("account registering endpoint", () => {
     await api.post("/accounts/").send(body);
 
     const response = await api.post("/auth/register/").send(body);
+
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe("login endpoint", () => {
+  beforeEach(async () => {
+    reconnect();
+    await connection.migrate.latest(configuration.migrations);
+  });
+
+  afterEach(async () => {
+    await connection.migrate.rollback(configuration.migrations);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("should return 200 and a token", async () => {
+    expect.assertions(2);
+
+    const body = {
+      email: "jorge@email.com",
+      password: "jorgepa55",
+    };
+    await api.post("/auth/register/").send({ name: "jorge", ...body });
+
+    const response = await api.post("/auth/login/").send(body);
+
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body.token).toBe("string");
+  });
+
+  it("should return 400 if email is wrong", async () => {
+    expect.assertions(1);
+
+    const body = {
+      email: "jorge@email.com",
+      password: "jorgepa55",
+    };
+    await api.post("/auth/register/").send({ name: "jorge", ...body });
+
+    const response = await api
+      .post("/auth/login/")
+      .send({ email: "wrong@email.com", password: body.password });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("should return 400 if password is wrong", async () => {
+    expect.assertions(1);
+
+    const body = {
+      email: "jorge@email.com",
+      password: "jorgepa55",
+    };
+    await api.post("/auth/register/").send({ name: "jorge", ...body });
+
+    const response = await api
+      .post("/auth/login/")
+      .send({ email: body.email, password: "wrongpa55" });
 
     expect(response.statusCode).toBe(400);
   });
