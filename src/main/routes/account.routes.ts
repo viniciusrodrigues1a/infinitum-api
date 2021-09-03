941909cdfb67ae71d82b4da657298aaace57aa19
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-09-03 10:24:29 -0300

    adicionando rota para Login

--- src/main/routes/account.routes.ts
+++ src/main/routes/account.routes.ts
@@ -5,8 +5,15 @@ import { knexControllerFactoryImpl } from "../factories/controllers";
 export const accountRoutes = Router();
 
 accountRoutes.post(
-  "/",
+  "/register",
   ExpressRouteAdapter((language) =>
     knexControllerFactoryImpl.makeRegisterController(language)
+  )
+);
+
+accountRoutes.post(
+  "/login",
+  ExpressRouteAdapter((language) =>
+    knexControllerFactoryImpl.makeLoginController(language)
   )
 );
