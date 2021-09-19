5b770b413e8bb5c72dc62f9d05ac0d22a1de0e4a
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-09-19 03:17:56 -0300

    criando tabela project_project_role no banco de dados

---
+++ src/shared/infra/database/migrations/20210919055705_create_account_project_project_role_table.js
@@ -0,0 +1,30 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("account_project_project_role", (table) => {
+    table
+      .uuid("account_id")
+      .notNullable()
+      .references("id")
+      .inTable("account")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .uuid("project_id")
+      .notNullable()
+      .references("id")
+      .inTable("project")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .uuid("project_role_id")
+      .notNullable()
+      .references("id")
+      .inTable("project_role")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table.primary(["account_id", "project_id", "project_role_id"]);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("account_project_project_role");
+};
